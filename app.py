f6a4f5315c276df484c9b89e240d42ee49b41fbb
abhilashkumar123 <56408160+abhilashkumar123@users.noreply.github.com>
2023-04-18 16:42:24 +0000

    Added dynamic data & API route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -28,6 +28,16 @@ JOBS = [{
 @app.route("/")
 def hello_world():
   return render_template('home.html', jobs=JOBS, Company_Name='Jovian Careers')
+
+
+#Get data in JSOn format and display in UI
+#https://jovian-careers-website.abhicvraman.repl.co/jobs
+#:- Will display data in jSON format.
+
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == "__main__":
